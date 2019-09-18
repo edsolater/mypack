5d03ce18725456b775a177097e3db26dfabc80bc
zhanggongying <zhanggongying@mrbs.pro>
2019-09-18 23:22:23 +0800

    增加新语义化函数invoke

--- utils.ts
+++ utils.ts
@@ -1,6 +1,6 @@
 /**
- * 分类：随机生成函数
- * 调用返回颜色字符串
+ * 工具函数——生成函数
+ * 返回随机生成的颜色字符串
  * @example
  * getRandomColor() // #123456
  * getRandomColor() // #EEFFEE
@@ -10,3 +10,13 @@ function getRandomColor() {
   return `#${Array.from({ length: 6 }, () => letters[Math.floor(Math.random() * 16)])}`
 }
 
+/**
+ *
+ * 简单的触发函数行为，给行为函数名增加业务代码的可读性
+ * @param fn 需触发的函数
+ * @param args 参数们
+ */
+function invoke<T>(fn: (...args: any[]) => T, ...args: any[]) {
+  if (typeof fn !== 'function') throw Error(`can't invoke ${fn}`)
+  return fn(...args)
+}
