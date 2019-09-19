0771a6008ef53154c2f63ecf5647a2f5d59a6871
edsolater <edsolater@qq.com>
2019-09-19 13:20:00 +0800

    加入tsconfig 更改名字

--- utils.ts
+++ utils.ts
@@ -2,10 +2,10 @@
  * 工具函数——生成函数
  * 返回随机生成的颜色字符串
  * @example
- * getRandomColor() // #123456
- * getRandomColor() // #EEFFEE
+ * makeRandomColor() // #123456
+ * makeRandomColor() // #EEFFEE
  */
-function generateRandomColor() {
+function makeRandomColor() {
   const letters = '0123456789ABCDEF';
   return `#${Array.from({ length: 6 }, () => letters[Math.floor(Math.random() * 16)])}`;
 }
@@ -25,7 +25,7 @@ function invoke<T>(fn: (...args: any[]) => T, ...args: any[]) {
  * @param count 生成的Iterator的可迭代数
  * @param mapFunc 初始化的map函数
  * @example 生成一组扑克牌
- *    const card = [...init(13, *(i)=>{
+ *    const card = [...generate(13, *(i)=>{
  *     const p = i + 1
  *      yield ['♠',p]
  *      yield ['♣',p]
@@ -33,7 +33,7 @@ function invoke<T>(fn: (...args: any[]) => T, ...args: any[]) {
  *      yield ['♦',p]
  *    }]
  */
-function* init(count, mapFunc = i => i) {
+function* generate(count, mapFunc = i => i) {
   for (let i = 0; i < count; i++) {
     if (mapFunc.constructor.name === 'GenertorFunction') {
       yield* mapFunc(i, count);
