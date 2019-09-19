6b9121fac0957c87afe6397a7e8838133635e27d
edsolater <edsolater@qq.com>
2019-09-19 09:39:31 +0800

    新增生成器函数

--- utils.ts
+++ utils.ts
@@ -5,9 +5,9 @@
  * getRandomColor() // #123456
  * getRandomColor() // #EEFFEE
  */
-function getRandomColor() {
-  const letters = '0123456789ABCDEF'
-  return `#${Array.from({ length: 6 }, () => letters[Math.floor(Math.random() * 16)])}`
+function generateRandomColor() {
+  const letters = '0123456789ABCDEF';
+  return `#${Array.from({ length: 6 }, () => letters[Math.floor(Math.random() * 16)])}`;
 }
 
 /**
@@ -17,6 +17,28 @@ function getRandomColor() {
  * @param args 参数们
  */
 function invoke<T>(fn: (...args: any[]) => T, ...args: any[]) {
-  if (typeof fn !== 'function') throw Error(`can't invoke ${fn}`)
-  return fn(...args)
+  if (typeof fn !== 'function') throw Error(`can't invoke ${fn}`);
+  return fn(...args);
+}
+/**
+ * 生成函数
+ * @param count 生成的Iterator的可迭代数
+ * @param mapFunc 初始化的map函数
+ * @example 生成一组扑克牌
+ *    const card = [...init(13, *(i)=>{
+ *     const p = i + 1
+ *      yield ['♠',p]
+ *      yield ['♣',p]
+ *      yield ['♥',p]
+ *      yield ['♦',p]
+ *    }]
+ */
+function* init(count, mapFunc = i => i) {
+  for (let i = 0; i < count; i++) {
+    if (mapFunc.constructor.name === 'GenertorFunction') {
+      yield* mapFunc(i, count);
+    } else {
+      yield mapFunc(i, count);
+    }
+  }
 }
