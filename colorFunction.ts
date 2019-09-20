27336eaebc57bdfa130782aaa88be418f91e9149
edsolater <edsolater@qq.com>
2019-09-20 17:10:24 +0800

    把所有关于色彩的函数提到一个单独的文件夹中；加入codetry.js 文件夹，方便调试用

--- colorFunction.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 仿照sass/less的颜色函数，以丰富JavaScript的底层库。
- */
-
-function toHSL(params: string) {
-  if (params.match(/#[0-9a-fA-F]{6}/)) {
-    
-  }
-}
--- utils.ts
+++ utils.ts
@@ -1,26 +1,4 @@
 import { baseFunc } from './lowerBaseFunctions';
-/**
- * 工具函数——生成函数
- * 返回随机生成的颜色字符串
- * @example
- * makeRandomColor() // #123456
- * makeRandomColor() // #EEFFEE
- */
-function makeRandomColor() {
-  const letters = '0123456789ABCDEF';
-  return `#${Array.from({ length: 6 }, () => letters[Math.floor(Math.random() * 16)])}`;
-}
-
-/**
- *
- * 简单的触发函数行为，给行为函数名增加业务代码的可读性
- * @param fn 需触发的函数
- * @param args 参数们
- */
-function invoke<T>(fn: (...args: any[]) => T, ...args: any[]) {
-  if (typeof fn !== 'function') throw Error(`can't invoke ${fn}`);
-  return fn(...args);
-}
 
 /**
  * 生成函数
