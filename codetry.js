932604715a97258a9b5c8f9f3a0bef8a2f375c29
edsolater <edsolater@qq.com>
2019-09-25 10:14:25 +0800

    导入css规定的颜色色值们（HSL）

--- codetry.js
+++ codetry.js
@@ -1,26 +1,41 @@
-function fromHEX(value) {
-  if (typeof value === 'string') {
-    let matchStr;
-    if ((matchStr = value.match(/^#([0-9a-fA-F]{7}|[0-9a-fA-F]{9})$/))) {
-      console.log('matchStr: ', matchStr);
-      if (matchStr.length === 6) {
-        // 例如: value = "#000fff"
-        return {
-          r: parseInt(matchStr.slice(0, 2), 16),
-          g: parseInt(matchStr.slice(2, 4), 16),
-          b: parseInt(matchStr.slice(4, 6), 16)
-        };
-      } else if (matchStr.length === 3) {
-        // 例如: value = "#3ef"
-        const dulp = n => n+n
-        return {
-          r: parseInt(dulp(matchStr.slice(0, 1)), 16),
-          g: parseInt(dulp(matchStr.slice(1, 2)), 16),
-          b: parseInt(dulp(matchStr.slice(2, 3)), 16)
-        };
-      }
-    }
-  }
-}
+/**
+ * RGB 颜色值转换为 HSL.
+ * 转换公式参考自 http://en.wikipedia.org/wiki/HSL_color_space.
+ * r, g, 和 b 需要在 [0, 255] 范围内
+ * 返回的 h, s, 和 l 在 [0, 1] 之间
+ *
+ * @param   Number  r       红色色值
+ * @param   Number  g       绿色色值
+ * @param   Number  b       蓝色色值
+ * @return  Array           HSL各值数组
+ */
+function rgbToHsl(r, g, b) {
+  (r /= 255), (g /= 255), (b /= 255);
+  var max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  var h,
+    s,
+    l = (max + min) / 2;
 
-console.log(fromHEX('#3ef'));
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+    h /= 6;
+  }
+
+  return [h, s, l];
+}
+console.log(rgbToHsl(235,236,235));
